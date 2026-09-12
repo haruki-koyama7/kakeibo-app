81a2a725ec264d0128faf3b71da3085386611322
haru0706 <haru.7766ko@gmail.com>
2026-09-12 22:15:21 +0900

    Fix: create instance folder on startup

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+instance_path = os.path.join(basedir, 'instance')
+os.makedirs(instance_path, exist_ok=True)  # フォルダが無ければ自動的に作成する
 
 class Config:
-    SECRET_KEY = 'dev-secret-key-change-later'  # あとで.envに移す
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'kakeibo.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-change-later')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(instance_path, 'kakeibo.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
