65298a327677b29dea03695353f1b337cf905d5a
haru0706 <haru.7766ko@gmail.com>
2026-09-12 22:35:37 +0900

    Fix: create tables on app startup for gunicorn

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ app.config.from_object(Config)
 
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'  # 未ログイン時に飛ばす先
